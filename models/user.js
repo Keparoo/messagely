2b80512f7448f2bfba7733a3b618bf56bff8b1e0
Kep <KepCreations@gmail.com>
2021-12-06 16:03:38 -0500

    Create updateLoginTime

--- models/user.js
+++ models/user.js
@@ -63,7 +63,19 @@ class User {
 
 	/** Update last_login_at for user */
 
-	static async updateLoginTimestamp(username) {}
+	static async updateLoginTimestamp(username) {
+		try {
+			const result = await db.query(
+				`UPDATE users
+                SET last_login_at current_timestamp
+                WHERE username = $1
+                RETURNING username`,
+				[ username ]
+			);
+		} catch (err) {
+			return next(err);
+		}
+	}
 
 	/** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
