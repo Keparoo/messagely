fdd5aceffb847f7d54285d1d1a2e0a217d478a23
Kep <KepCreations@gmail.com>
2021-12-06 21:19:28 -0500

    Create routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,32 @@
+const express = require('express');
+const router = new express.Router();
+const jwt = require('jsonwebtoken');
+
+const ExpressError = require('../expressError');
+const db = require('../db');
+const User = require('./users');
+const { authenticate, updateLoginTimestamp } = require('../models/user');
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
 
+router.post('/login', async function(req, res, next) {
+	try {
+		const { username, password } = req.body;
+
+		if (await User.authenticate(username, password)) {
+			const token = jwt.sign({ username }, SECRET_KEY);
+			User.updateLoginTimestamp(username);
+			return res.json({ token });
+		}
+		throw new ExpressError('Invalid user/password', 400);
+	} catch (err) {
+		return next(err);
+	}
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +34,15 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post('/register', async function(req, res, next) {
+	try {
+		const { username } = User.register(req.body);
+		const token = jwt.sign({ username }, SECRET_KEY);
+		User.updateLoginTimestamp(username);
+		return res.json({ token });
+	} catch (err) {
+		return next(err);
+	}
+});
+module.exports = router;
